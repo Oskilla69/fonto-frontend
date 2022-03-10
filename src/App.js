b6490732307a668bad3441e2b3eca3b51b50c4f4
oskilla68 <oscar.fan1@gmail.com>
2022-03-10 23:17:51 +1100

    remove unnecessary stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const headers = { 'x-hasura-user-id': 'OFy22JCTOKbqrsF' }
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch('https://take-home.hasura.app/api/rest/properties/all', {headers}).then(res => {
+      if(res.ok) {
+        return res.json()
+      } else {
+        console.log("error in API")
+      }
+    }).then(data => {
+      setData(data)
+    })
+  }, [])
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Properties</h1>
       </header>
+      <main>
+        <button>Add Property</button>
+        <table>
+          <tr>
+            <th>ID</th>
+            <th>Address</th>
+            <th>Valuation</th>
+          </tr>
+          {data && data.properties.map(row => {
+            if(!row.hidden) {
+              return <tr><td>{row.id}</td><td>{row.address}</td><td>{row.valuation}</td></tr>
+            }
+        })}
+        </table>
+      </main>
     </div>
   );
 }
